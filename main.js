224d9a1451bf946340c1876ae31937ffb12fc340
Farah <farah.hsh.12@gmail.com>
2023-08-14 16:18:41 +0300

    day2

---
+++ main.js
@@ -0,0 +1,66 @@
+// day2 variables and const lesson
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+//part 1 of the lesson variable
+
+// learning
+//way 1 to introduce variable explicit
+let Username="javascript";
+
+
+// way 2 to introduce variable implicit
+let username;
+username="javascript1";
+// when changing the variable 
+username="farah";
+
+
+// rule : you cant use words that are already taken
+// let break
+
+// var is old doesn't show error when variable is changed
+var mYname="farah";
+var mYname="jumanah";
+
+// let is new it shows error when variable is changed
+// let myname="farah";
+// let myname="jumanah";
+
+
+
+
+// practice1
+
+// explicit
+let tuwaiq1="tuwaiq"
+console.log(tuwaiq1)
+
+
+// implicit
+let tuwaiq;
+tuwaiq="tuwaiq"
+console.log(tuwaiq)
+
+
+//part 2 of the lesson const
+
+// learning
+
+// const doesn't change always fixed 
+
+// explicit way 
+const placeofbirth="jubail"
+
+// implicit way doesn't work for const only in let
+// const place_ofbirth;
+// place_ofbirth="jubail";
+
+// practice2
+let name="farah alshahrani"
+const birthyear="2008"
+
+console.log(name)
+console.log(birthyear)
+
+
